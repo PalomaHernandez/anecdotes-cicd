ccb0faa868864702e11d3e466b5587389fcc2ab8
p hernandez <palohernandez01@gmail.com>
2025-07-07 11:08:10 -0300

    Update playwright config and pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run prod",
+    command: "npm --prefix backend run start:test",
     url: "http://localhost:3001",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
