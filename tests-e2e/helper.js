ccf4268b3578f728090df537ee0dfbe6f4a29052
p hernandez <palohernandez01@gmail.com>
2025-07-07 16:18:54 -0300

    Debug logs

--- tests-e2e/helper.js
+++ tests-e2e/helper.js
@@ -2,6 +2,11 @@ const loginWith = async (page, username, password) => {
   await page.getByTestId("username").fill(username);
   await page.getByTestId("password").fill(password);
   await page.getByRole("button", { name: "Log in" }).click();
+
+  const token = await page.evaluate(() =>
+    localStorage.getItem("loggedBlogappUser")
+  );
+  console.log("Token guardado en localStorage:", token);
 };
 
 const createBlog = async (page, title, author, url) => {
